d6707217c237cb90ede79517abd8a2b90c42495d
randy_savage <42255766+CreamOfTheCrop-01@users.noreply.github.com>
2025-09-16 16:03:02 +1000

    Update streamlit_app.py
    change to lowercase 's' in 'snowflake'

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from snowflake.snowpark.functions import col;
 st.title(f":cup_with_straw: Customize your Smoothie!:cup_with_straw:")
 st.write("Choose the fruits you want in your custom smoothie!")
 
-cnx = st.connection("Snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
